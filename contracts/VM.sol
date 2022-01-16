ec39548e32a70141497d5a158ed5c477d43986c7
Andrew Redden <opensource@andrewredden.com>
2022-01-16 18:19:46 -0400

    allowlist poc

--- contracts/VM.sol
+++ contracts/VM.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.4;
 
 import "./CommandBuilder.sol";
-
 uint8 constant FLAG_CT_DELEGATECALL = 0x00;
 uint8 constant FLAG_CT_CALL = 0x01;
 uint8 constant FLAG_CT_STATICCALL = 0x02;
@@ -14,10 +13,82 @@ uint8 constant FLAG_TUPLE_RETURN = 0x40;
 
 uint256 constant SHORT_COMMAND_FILL = 0x000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
+
+contract Registry {
+    mapping(address => mapping(bytes4 => bool)) public isActiveCommand;
+
+    mapping(bytes32 => bool) public byteCodeAllowList;
+
+    address governance;
+
+    address futureGovernance;
+
+    modifier onlyGovernance() {
+        require(address(msg.sender) == governance);
+        _;
+    }
+
+    constructor(address multiSig) {
+        governance = multiSig;
+    }
+
+    function transferGovernance(address governance) external onlyGovernance {
+        futureGovernance = governance;
+    }
+
+    function acceptGovernance() external {
+        require(msg.sender == futureGovernance, "must be futureGovernance to accept");
+        governance = futureGovernance;
+        futureGovernance = address(0);
+    }
+
+    function isValidCommand(address contractAddress, bytes4 selector)
+    public
+    view
+    returns (bool valid) {
+        bytes32 codeHash;
+        assembly {codeHash := extcodehash(externalContract)}
+
+        valid = (byteCodeAllowList[codeHash] && isActiveCommand[contractAddress][selector]);
+    }
+
+    function registerNewCommands(address[] contractsToEnable, bytes4[] selectorsToEnable)
+    public
+    onlyGovernance {
+        require(contractsToEnable.length == selectorsToEnable.length, "invalid length mismatch");
+        for (uint i = 0; i < contractsToEnable.length; i++) {
+            _registerNewContract(contractsToEnable[i], selectorsToEnable[i]);
+        }
+    }
+
+    modifier onlyGovernance() {
+        require(address(msg.sender) == guardian);
+        _;
+    }
+    function _registerNewContract(address externalContract, bytes4 selector) external onlyGovernance {
+        bytes32 codeHash;
+        assembly {codeHash := extcodehash(externalContract)}
+
+        require(!isActiveCommand[externalContract][selector], "Command already added");
+
+        byteCodeAllowList[codeHash] = true; //set into its own function maybe for 2 step activation, code and then contract/selector
+        isActiveCommand[externalContract][selector] = true;
+    }
+}
+
+interface IRegistry {
+    function isValidCommand(address contractAddress, bytes4 selector)
+    public
+    view
+    returns (bool valid);
+}
+
 contract VM {
     using CommandBuilder for bytes[];
 
     address immutable self;
+
+    IRegistry activeCommands = IRegistry(0x12345678901010); //set static
 
     modifier ensureDelegateCall() {
         require(address(this) != self);
@@ -29,9 +100,9 @@ contract VM {
     }
 
     function execute(bytes32[] calldata commands, bytes[] memory state)
-        public
-        ensureDelegateCall
-        returns (bytes[] memory)
+    public
+    ensureDelegateCall
+    returns (bytes[] memory)
     {
         bytes32 command;
         uint256 flags;
@@ -42,6 +113,8 @@ contract VM {
 
         for (uint256 i = 0; i < commands.length; i++) {
             command = commands[i];
+            //address, selector
+            require(allowedCommands.isValidCommand(address(uint160(uint256(command))), bytes4(command)), "command is not authorized");
             flags = uint8(bytes1(command << 32));
 
             if (flags & FLAG_EXTENDED_COMMAND != 0) {
@@ -53,18 +126,18 @@ contract VM {
             if (flags & FLAG_CT_MASK == FLAG_CT_DELEGATECALL) {
                 (success, outdata) = address(uint160(uint256(command))) // target
                 .delegatecall(
-                    // inputs
+                // inputs
                     state.buildInputs(
-                        //selector
+                    //selector
                         bytes4(command),
                         indices
                     )
                 );
             } else if (flags & FLAG_CT_MASK == FLAG_CT_CALL) {
-                (success, outdata) = address(uint160(uint256(command))).call( // target
-                    // inputs
+                (success, outdata) = address(uint160(uint256(command))).call(// target
+                // inputs
                     state.buildInputs(
-                        //selector
+                    //selector
                         bytes4(command),
                         indices
                     )
@@ -72,9 +145,9 @@ contract VM {
             } else if (flags & FLAG_CT_MASK == FLAG_CT_STATICCALL) {
                 (success, outdata) = address(uint160(uint256(command))) // target
                 .staticcall(
-                    // inputs
+                // inputs
                     state.buildInputs(
-                        //selector
+                    //selector
                         bytes4(command),
                         indices
                     )
@@ -85,12 +158,12 @@ contract VM {
                 assembly {
                     mstore(calleth, add(v, 0x20))
                 }
-                (success, outdata) = address(uint160(uint256(command))).call{ // target
-                    value: calleth
+                (success, outdata) = address(uint160(uint256(command))).call{// target
+                value : calleth
                 }(
-                    // inputs
+                // inputs
                     state.buildInputs(
-                        //selector
+                    //selector
                         bytes4(command),
                         bytes32(uint256(indices << 8) | IDX_END_OF_ARGS)
                     )
